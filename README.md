5415a43982bd19be24b10ab161aad13496bc6e1b
Francisco Neto <francisconetodata@gmail.com>
2022-06-30 22:15:18 -0300

    Primeiro Commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# pycaret-fast-api-HeartDisease
+# Pycaret - FAST API exemplo
 Exemplo de criação de API com o Pycaret - predição de HeartDisease
---
+++ heartprediction.py
@@ -0,0 +1,22 @@
+
+import pandas as pd
+from pycaret.classification import load_model, predict_model
+from fastapi import FastAPI
+import uvicorn
+
+# Create the app
+app = FastAPI()
+
+# Load trained Pipeline
+model = load_model('heartprediction')
+
+# Define predict function
+@app.post('/predict')
+def predict(Age, Sex, ChestPainType, RestingBP, Cholesterol, FastingBS, RestingECG, MaxHR, ExerciseAngina, Oldpeak, ST_Slope):
+    data = pd.DataFrame([[Age, Sex, ChestPainType, RestingBP, Cholesterol, FastingBS, RestingECG, MaxHR, ExerciseAngina, Oldpeak, ST_Slope]])
+    data.columns = ['Age', 'Sex', 'ChestPainType', 'RestingBP', 'Cholesterol', 'FastingBS', 'RestingECG', 'MaxHR', 'ExerciseAngina', 'Oldpeak', 'ST_Slope']
+    predictions = predict_model(model, data=data) 
+    return {'prediction': list(predictions['Label'])}
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=8000)
